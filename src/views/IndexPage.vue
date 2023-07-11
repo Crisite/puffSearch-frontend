f35a591fc28c10a347dd8542c74db93d352857a0
puff <844858692@qq.com>
2023-07-12 00:47:57 +0800

    frontend init

---
+++ src/views/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+      v-model:value="searchText"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const searchText = ref("");
+
+const onSearch = (value: string) => {
+  alert(value);
+};
+</script>
